310bfab0c9cf185cfbcb030f3734ab3250f90e72
Jerrison <jerryyip24@gmail.com>
2021-07-16 14:18:43 -0400

    Finished Project, added AI - skipped design

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tic-Tac-Toe
 The Odin Project - Tic Tac Toe 
+
+Implemented AI - Have not incorporated Minimax as a harder AI 
+
+Learned that '.children' accesses all children elements of the parent (only 1 layer deep)
--- js/script.js
+++ js/script.js
@@ -1,49 +1,109 @@
 const playerOneButton = document.querySelector('.player1');
 const playerTwoButton = document.querySelector('.player2');
 const resetButton = document.querySelector('.reset');
+const twoPlayerBtn = document.querySelector('.twoPlayer');
+const vsComputerBtn = document.querySelector('.vsComputer');
+const mainContainer = document.querySelector('main');
+const introContainer = document.querySelector('.intro-card');
 
 let player1 = 'Player 1';
 let player2 = 'Player 2';
+let gameBoard = ['','','','','','','','','',];
 
 // Buttons to change player names
 playerOneButton.addEventListener('click', () => {
-    player1 = prompt('Player 1, what is your name?')
+    player1 = prompt(`${player1}, what is your name?`)
 })
 playerTwoButton.addEventListener('click', () => {
-    player2 = prompt('Player 2, what is your name?')
+    player2 = prompt(`${player2}, what is your name?`)
 })
 
+// Button to reset game - return to front
 resetButton.addEventListener('click', () => {
     clearBoard();
+    // mainContainer.classList.add('display-blur');
+    // introContainer.classList.remove('display-none');
 })
 
+// intro selection - 2 player
+twoPlayerBtn.addEventListener('click', () => {
+    mainContainer.classList.remove('display-blur');
+    introContainer.classList.add('display-none');
+    twoPlayer();
+})
 
-let gameBoard = ['','','','','','','','','',];
-
+// intro selection - vs Computer
+vsComputerBtn.addEventListener('click', () => {
+    mainContainer.classList.remove('display-blur');
+    introContainer.classList.add('display-none');
+    vsComputer();
+})
 
 const cellContainer = document.querySelector('.gameboard');
 const gameCell = document.querySelector('.cell')
 
-// Each button event listener
-Array.from(cellContainer.children).forEach((gameCell) => {
-    let cellNum = Number(gameCell.dataset.num); // grabs the dataset value and converts to number
+// Function for button event listener for 2 player
+let twoPlayer = () => {
+    Array.from(cellContainer.children).forEach((gameCell) => {
+        let cellNum = Number(gameCell.dataset.num); // grabs the dataset value and converts to number
 
-    gameCell.addEventListener('click', () => {
-        if (gameBoard[cellNum] === '') { // stops multiple clicks to overwrite previous value
-            // check array empty strings is odd/even, outputs depending
-            if ((gameBoard.length - gameBoard.filter(String).length) % 2 === 0 ) { 
-                gameBoard[cellNum] = 'X'; // gameBoard[cell you clicked] is now 'X' and then rune drawGameBoard() to refresh output
-                drawGameBoard(gameBoard);
-                checkWinner(gameBoard);
-            } else {
+        gameCell.addEventListener('click', () => {
+            if (gameBoard[cellNum] === '') { // stops multiple clicks to overwrite previous value
+                // check array empty strings is odd/even, outputs depending
+                if ((gameBoard.length - gameBoard.filter(String).length) % 2 === 0 ) { 
+                    gameBoard[cellNum] = 'X'; // gameBoard[cell you clicked] is now 'X' and then rune drawGameBoard() to refresh output
+                    drawGameBoard(gameBoard);
+                    checkWinner(gameBoard);
+                } else {
+                    gameBoard[cellNum] = 'O';
+                    drawGameBoard(gameBoard);
+                    checkWinner(gameBoard);
+                }
+            }
+        })
+    })
+}
+
+// Function for button event listener for vs Computer 
+let vsComputer = () => {
+    player1 = 'User';
+    player2 = 'Computer';
+    playerOneButton.textContent = 'Player Name';
+    playerTwoButton.textContent = 'Computer Name';
+
+    Array.from(cellContainer.children).forEach((gameCell) => {
+        let cellNum = Number(gameCell.dataset.num); // grabs the dataset value and converts to number
+
+        gameCell.addEventListener('click', () => {
+            if (gameBoard[cellNum] === '') { // stops multiple clicks to overwrite previous value
                 gameBoard[cellNum] = 'O';
                 drawGameBoard(gameBoard);
                 checkWinner(gameBoard);
+                computerMove();
             }
-        }
+        })
     })
-})
+}
 
+// Function to determine computers move
+let computerMove = () => {
+    let num = randomNum()
+
+    if (gameBoard[num] === '') {
+        gameBoard[num] = 'X';
+        drawGameBoard(gameBoard);
+        checkWinner(gameBoard);
+    } else {
+        randomNum()
+        computerMove()
+    }
+}
+
+// function for random number
+let randomNum = () => {
+    let randomNum = Math.floor(Math.random() * 8);
+    return randomNum;
+}
 
 // Function to run the array and output elements into HTML
 let drawGameBoard = (board) => {
@@ -56,7 +116,7 @@ let drawGameBoard = (board) => {
 // Function to reset board
 let clearBoard = () => {
     gameBoard = ['','','','','','','','','',];
-    drawGameBoard(gameBoard);
+    drawGameBoard(gameBoard); 
 }
 
 // Function to check winner using Brute Force method
